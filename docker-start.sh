e9f5af7d601bb7952af99c56d7844cd5e789774a
Alex <hernandezmuley@gmail.com>
2026-02-23 17:45:37 +0100

    Fix asset loading and URLs for production

--- docker-start.sh
+++ docker-start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# Map Render URL to Laravel APP_URL if not set
+if [ -n "$RENDER_EXTERNAL_URL" ] && [ -z "$APP_URL" ]; then
+    export APP_URL=$RENDER_EXTERNAL_URL
+fi
+
 # Run migrations
 php artisan migrate --force
 
